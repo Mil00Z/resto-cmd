de57b14d7a218dfd00fe69c84deca9e74e48fe26
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-01-10 13:38:29 +0100

    create action + reducer with builder function

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createReducer,createAction } from '@reduxjs/toolkit'
 
-import {PouletCroquant, SuperCremeux, DoubleCantal} from '../common/models'
+// import {PouletCroquant, SuperCremeux, DoubleCantal} from '../common/models'
 
 // Initial Flow
 let state = {
@@ -9,31 +9,86 @@ let state = {
   list:[],
 };
 
+export const addProduct = createAction('ADD_PRODUCT');
+export const addVoucher = createAction('APPLY_VOUCHER');
+export const updateName = createAction('UPDATE_FIRSTNAME');
+export const removeProduct = createAction('REMOVE_PRODUCT');
+
+
+const reducer = createReducer(state, (builder) => {
+  builder
+    .addCase(addProduct, (state, action) => {
+      const listWithNewProduct = [...state.list, action.payload];
+      return { ...state, list: listWithNewProduct };
+    })
+    .addCase(addVoucher, (state, action) => {
+      const withVoucherList = state.list.map(
+        (item) => (item.title === 'Super Crémeux' ? { ...item, price: action.payload.price } : item)
+      );
+      return { ...state, list: withVoucherList };
+    })
+    .addCase(removeProduct, (state, action) => {
+      const list = state.list.filter((item, index) => index !== action.payload);
+      return { ...state, list: list };
+    })
+    .addCase(updateName, (state, action) => {
+      const owner = { ...state.owner, firstName: action.payload };
+      return { ...state, owner };
+    });
+});
+
+
+// const reducer = createReducer(state, {
+  
+//     [addProduct] : (currentState,action) => {
+
+//       const listWithNewProduct = [...currentState.list, action.payload]
+//       return {...currentState, list: listWithNewProduct}
+//     },
+//    [addVoucher] : (currentState,action) => {
+
+//     const withVoucherList = currentState.list.map(
+//       item => item.title === 'Super Crémeux' ? ({...item, price: action.payload.price}) : item)
+//       return {...currentState, list: withVoucherList}
+//   }, 
+//   [removeProduct] : (currentState,action) => {
+
+//     const list = currentState.list.filter((item, index) => index !== action.payload)
+//     return {...currentState, list: list}
+//   },
+//   [updateName] :(currentState,action) => {
+
+//     const owner = {...currentState.owner, firstName: action.payload}
+//     return {...currentState, owner}
+
+//   }
+// });
+
 
 // Flux
-const reducer = (currentState, action ) => {
-  switch (action.type) {
-      case 'ADD_PRODUCT':
-          const listWithNewProduct = [...currentState.list, action.payload]
-          return {...currentState, list: listWithNewProduct}
-      case 'REMOVE_PRODUCT':
-          const list = currentState.list.filter(
-(item, index) => index !== action.payload
-          )
-          return {...currentState, list: list}
-      case 'APPLY_VOUCHER':
-          const withVoucherList = currentState.list.map(
-              item => item.title === 'Super Crémeux' ? ({...item, price: action.payload.price}) : item
-          )
-          return {...currentState, list: withVoucherList}
+// const reducer = (currentState, action ) => {
+//   switch (action.type) {
+//       case 'ADD_PRODUCT':
+//           const listWithNewProduct = [...currentState.list, action.payload]
+//           return {...currentState, list: listWithNewProduct}
+//       case 'REMOVE_PRODUCT':
+//           const list = currentState.list.filter(
+// (item, index) => index !== action.payload
+//           )
+//           return {...currentState, list: list}
+//       case 'APPLY_VOUCHER':
+//           const withVoucherList = currentState.list.map(
+//               item => item.title === 'Super Crémeux' ? ({...item, price: action.payload.price}) : item
+//           )
+//           return {...currentState, list: withVoucherList}
 
-      case 'UPDATE_FIRSTNAME':
-          const owner = {...currentState.owner, firstName: action.payload}
-          return {...currentState, owner}
-      default:
-          return currentState
-  }
-}
+//       case 'UPDATE_FIRSTNAME':
+//           const owner = {...currentState.owner, firstName: action.payload}
+//           return {...currentState, owner}
+//       default:
+//           return currentState
+//   }
+// }
 
 
 //Store Initialization
