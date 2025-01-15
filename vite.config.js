9bfdf3e0c18ef1413c84af1c9a5424ef5b7ce9be
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-01-15 11:18:36 +0100

    add shortcut vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  alias : {
+    '@root' : '/src',
+    '@common' : '/src/common',
+    '@components' : '/src/components',
+    '@app' : '/src/app',
+    '@store' : '/src/app/store',
+    '@selector' : '/src/app/selector',
+  }
 })
