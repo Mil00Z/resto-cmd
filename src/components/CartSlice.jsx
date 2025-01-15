a2ab00adb1326f548b24ca29f087af7e71e34d65
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-01-15 16:03:55 +0100

    quick change on thunk exemple

--- src/components/CartSlice.jsx
+++ src/components/CartSlice.jsx
@@ -6,77 +6,11 @@ import * as ProductList from '../common/models';
 
 
 
-
-
-//Thunk
-// const addProductThunk = (product) => (dispatch,getState) => {
-
-//   //Dispatch new product directly to the store
-//   dispatch(cartSlice.actions.addProduct(product));
-  
-//   //Makesomething in background (like async or anything else)
-//   return new Promise((resolve,reject) => {
-
-//     setTimeout(()=>{
-
-//       const state = getState();
-
-//       //List d'element initiaux avec quantity (update ou non)
-//       const numberProductList = getListQuantityProductPerName(state);
-
-//       // Trouver le premier element qui match avec la condition "name"
-//       const numberProductWithOffer =  numberProductList.find((element) => element.title === 'Double Cantal').quantity;
-
-//       // console.log(numberProductList,numberProductWithOffer)
-
-//     //Si déjà n produits dans la liste, alors je proposes une offre
-//     if (numberProductWithOffer === 2){
-
-//       window.confirm('voulez vous rajouter un 3eme burger identique avec reduction ');
-
-    
-//       dispatch(cartSlice.actions.addProduct({...DoubleCantal,price:8}));
-
-//   }
-  
-
-
-//       resolve()
-
-//     },2000);
-
-//   })  
-// }
-
-
-export const resetOrderThunk = createAsyncThunk('cart/resetOrderThunk', async (thunkApi) => {
-  
-return new Promise((resolve,reject) => {
-
-  setTimeout(()=>{
-
-    console.log('thunk log 1',thunkApi)
-
-    if(window.confirm('attention, vous voulez annuler votre commande ?')){
-
-      console.log('thunk log 2',thunkApi)
-
-      reject()
-
-    }
-
-  },10000);
-
-  })
-
-});
-
-
-
 export const addProductThunk = createAsyncThunk('cart/addProductThunk', async (product,thunkApi) => {
 
  thunkApi.dispatch(cartSlice.actions.addProduct(product))
- thunkApi.dispatch(resetOrderThunk());
+
+//  thunkApi.dispatch(resetOrderThunk());
 
  return new Promise((resolve,reject) => {
 
@@ -88,7 +22,7 @@ export const addProductThunk = createAsyncThunk('cart/addProductThunk', async (p
       const numberProductList = getListQuantityProductPerName(state);
 
       // Trouver le premier element qui match avec la condition "name"
-      const numberProductWithOffer =  numberProductList.find((element) => element.title === 'Double Cantal').quantity;
+      const numberProductWithOffer =  numberProductList.find((element) => element.title === 'Poulet Croquant').quantity;
 
       // console.log(numberProductList,numberProductWithOffer)
 
@@ -115,9 +49,29 @@ export const addProductThunk = createAsyncThunk('cart/addProductThunk', async (p
 });
 
 
+export const resetOrderThunk = createAsyncThunk('cart/resetOrderThunk', async (thunkApi) => {
+  
+  return new Promise((resolve,reject) => {
+  
+    setTimeout(()=>{
+  
+      console.log('thunk log 1',thunkApi)
+  
+      if(window.confirm('attention, vous voulez annuler votre commande ?')){
+  
+        console.log('thunk log 2',thunkApi)
+  
+        reject()
+  
+      }
+  
+    },10000);
+  
+    })
+  
+  });
 
-
-
+  
 
 export const cartSlice = createSlice({
   name:"list",
@@ -145,7 +99,7 @@ export const cartSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(addProductThunk.fulfilled, (state) => {
-        const specialOffer = ProductList.DoubleCantal;
+        const specialOffer = ProductList.PouletCroquant;
             return [...state, {...specialOffer, price:8}]
       })
       .addCase(addProductThunk.rejected, (state) => {
